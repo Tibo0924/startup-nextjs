740a963c950d08f110653948b272e3d0d94f6dae
Tibor <tiborkeri@icloud.com>
2023-08-07 11:35:45 +0200

    Add analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
+import { Analyitics } from '@vercel/analytics'
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <Providers>
           <Header />
           {children}
+          {Analyitics}
           <Footer />
           <ScrollToTop />
         </Providers>
